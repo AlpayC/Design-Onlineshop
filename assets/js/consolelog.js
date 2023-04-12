038803300f78cda707c4c2f031d3a1e7c7fcb0dc
AlpayC <alpaycelik91@googlemail.com>
2023-04-12 16:23:36 +0200

    Add js script to an external file in path assets/js

---
+++ assets/js/consolelog.js
@@ -0,0 +1,7 @@
+function sConsole(event) {
+event.preventDefault();
+var data1 = document.getElementById("data");
+var data2 = document.getElementById("mail");
+console.log(data1.value);
+console.log(data2.value);
+}
